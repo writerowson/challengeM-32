797578afc4696f10cdc781cd2a7bb301c430c255
coderowson <runurowson@gmail.com>
2022-02-21 16:03:09 +0600

    probem three

--- 0neTW0.js
+++ 0neTW0.js
@@ -19,4 +19,29 @@ student = roll3 + ',' + roll4
 console.log(student)
 
 // ২. টেম্পলেট স্ট্রিং দিয়ে একটা স্ট্রিং তৈরি করো। সেটার মধ্যে উপরে ডিক্লেয়ার করা ভেরিয়েবল এর মান ডাইনামিক ভাবে বসাও। একইভাবে উপরে একটা অবজেক্ট ডিক্লেয়ার করো। এবং ডাইনামিকভাবে উপরের অবজেক্ট এর দুইটা প্রপার্টি এর মান তোমার টেমপ্লেট স্ট্রিং এর মধ্যে বসাও। 
+let roomMate = 'Disgusting Woman'
+let position = 'talking'
+let update = `${roomMate} ${position}`
+console.log(update)
+
+const introduction = {
+    nam: 'Queen',
+    age: 27,
+    address: "Ajabpur",
+    qalification: 'illiterate',
+    occupation: 'sleeping'
+}
+const introduc = {
+    nam: 'Rowdhon Ara',
+    age: 27,
+    address: "Khulna",
+    qalification: 'graduation',
+    occupation: 'freelancer'
+}
+
+const paragraph = `Once upon a time there was a ${introduction.nam}. Her age was ${introduction.age}. She lives in ${introduction.address}.`
+console.log(paragraph)
+const para = `Once upon a time there was a ${introduc.nam}. Her age was ${introduc.age}. She lives in ${introduc.address}. Now she is a ${introduc.occupation}`
+console.log(para)
+
 
---
+++ three.js
@@ -0,0 +1,40 @@
+// ৩.১ একটা প্যারামিটার ওয়ালা arrow ফাংশন ডিক্লেয়ার করো। এবং সেই ফাংশনের কাজ হবে তুমি কোন ইনপুট দিলে সেই ইনপুট সংখ্যাকে ৫ দিয়ে ভাগ করে আউটপুট দিবে। 
+const bil = rent => rent / 5
+const reeesult = bil(20)
+// console.log(result)
+// or
+const bill = x => x / 5
+const result = bill(50)
+console.log(result)
+
+
+// ৩.২ তুমি দুইটা প্যারামিটার ওয়ালা একটা অ্যারো ফাংশন লিখবে। সেই ফাংশনের ভিতরে কাজ হবে। প্রত্যেকটা ইনপুট প্যারামিটার এর সাথে ২ যোগ করবে তারপর যোগফল দুইটা গুণ করবে। ক্যামনে করবে সেটা চিন্তা করে বের করার চেষ্টা করো
+// normal functon
+function sister(ranu, runu) {
+    let sum1 = ranu + 2;
+    let sum2 = runu + 2
+    return multiply = sum1 * sum2
+}
+let final = sister(1, 3)
+console.log(final)
+
+// arrow function
+const sis = (rani, runa) => (rani + 2
+) * (runa + 2)
+const value = sis(1, 3)
+console.log(value)
+
+
+// ৩.৩ এইবার তিনটা প্যারামিটার ওয়ালা একটা অ্যারো ফাংশন ডিক্লেয়ার করো। যেই ফাংশনের কাজ হবে তিনটা প্যারামিটার নিয়ে সেই তিনটা প্যারামিটারকে গুণ করে সেই রেজাল্ট রিটার্ন করবে।
+
+const family = (Ma, Baba, Bachha) => Ma * Baba * Bachha
+const home = family(3, 5, 1)
+console.log(home)
+
+
+
+// ৩.৪ এইবার দুইটা প্যারামিটার ওয়ালা একটা অ্যারো ফাংশন নিবে। ওই arrow ফাংশনটা হবে অনেকগুলা লাইনের। সেখানে প্রত্যেকটা ইনপুট প্যারামিটার এর সাথে ২ যোগ করবে তারপর যোগফল দুইটা গুণ করবে। ক্যামনে করবে সেটা চিন্তা করে বের করার চেষ্টা করো। 
+
+const skill = (webDevelopment, graphics) => ((webDevelopment + 2) * (graphics + 2) {
+
+} 
